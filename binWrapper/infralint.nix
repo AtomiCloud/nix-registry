69942b0c8f52bf24d5aa84f76e52c5b7aec2c318
kirinnee <kirinnee97@gmail.com>
2025-02-11 11:53:28 +0800

    update(infralint): include terraform tools
    - added terraform-docs
    - added tfsec
    - added tflint
    
    to enhance the linting capabilities for Terraform configurations by including
    additional tools that improve code quality and security.

--- binWrapper/infralint.nix
+++ binWrapper/infralint.nix
@@ -6,9 +6,17 @@ pkgs.runCommand "infralint"
   buildInputs = [
     pkgs.hadolint
     pkgs.helm-docs
+
+    pkgs.terraform-docs
+    pkgs.tfsec
+    pkgs.tflint
   ];
 } ''
   mkdir -p $out/bin
   cp ${pkgs.hadolint}/bin/* $out/bin/
   cp ${pkgs.helm-docs}/bin/* $out/bin/
+
+  cp ${pkgs.terraform-docs}/bin/* $out/bin/
+  cp ${pkgs.tfsec-docs}/bin/* $out/bin/
+  cp ${pkgs.tflint-docs}/bin/* $out/bin/
 ''
